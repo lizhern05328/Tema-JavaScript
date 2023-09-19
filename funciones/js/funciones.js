7f35ed576ba8d29e82fd0ed4d6da6d318a962f66
lizhern05328 <lizh05328@gmail.com>
2023-09-19 13:56:32 -0600

    Ejercicios resueltos

---
+++ funciones/js/funciones.js
@@ -0,0 +1,26 @@
+// calcular el perimetro de un circulo
+let pi = 3.1416;
+
+function periCirculo (diametro){
+    return  pi * diametro;
+}
+
+// calcular el area de un rectangulo
+
+function areaRec (base, altura){
+    return  base * altura;
+}
+
+// calcular el cuadrado de un numero
+
+function squNumber (number){
+    return  number * number;
+}
+
+
+// calcular la conversion de grados celsius a farenheit
+
+
+// calcular el valor del voltaje dadas la resistencia y la corriente
+
+//  calcular el volumen de una esfera
---
+++ variables-datos/js/variables.js
@@ -0,0 +1,53 @@
+/* Declarar 10 variables de cada tipo de 
+dato sin tomar en cuenta objetos, arrays y null*/
+
+var numUser = 23;
+var nameUser = "Ana";
+var femalerUser = true;
+var isExist = false;
+var lastName = "Gomez Herrera";
+let movieFvorite = "Alicia en el Pais de la maravillas";
+const name = 'Alex Canton';
+const edgeUser = 23;
+let countryUser;
+let phoneNumber = 55444333;
+
+/* * Declarar variables numericas y realizar 
+las operaciones aritmeticas basicas
+ */
+
+var num1 = 10;
+var num2 = 50;
+
+//suma
+console.log('Suma');
+let resulSuma = num1 + num2;
+console.log(resulSuma);
+
+// resta 
+console.log('Resta');
+let resulResta = num2 - num1;
+console.log(resulResta);
+
+// multiplicacion
+console.log('Multiplicación');
+let resulMul = num1 * num2;
+console.log(resulMul);
+
+//Division
+console.log('División');
+let resulDiv = num2 / num1;
+console.log(resulDiv);
+
+/*  Declarar variables que concatenen cadenas de texto*/
+
+let saludo = " Buenos dias";
+let nameCountry = "Colombia";
+let from = " desde de "
+
+let sayHellow = saludo + nameUser + from + nameCountry;
+console.log( sayHellow);
+
+
+
+
