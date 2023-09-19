3baddd061233c9faab2c52d558d260397c5bbc84
lizhern05328 <lizh05328@gmail.com>
2023-09-19 17:23:30 -0600

    Ejercicios de Control

--- control/js/control.js
+++ control/js/control.js
@@ -72,3 +72,27 @@ if( (num / 7) == 0 && (num / 8) == 0){
  * Realizar un programa que reciba como parametro una categoria de peliculas
  */
 
+let typeFilm = "terror";
+switch (typeFilm){
+    case "accion" : 
+        console.log("Duro de matar\nEscape bajo fuejo\nJohn Wick");
+        break;
+    case "drama" :
+        console.log("La ballena\nEl gran Pez\nLA LA LAND");
+        break;
+    case "comedia" : 
+        console.log("CODA\nQue paso Ayer\nThe Break-Up");
+        break;
+    case "romance" :
+        console.log("All my Life\nMi primer amor\nThe Notebook");
+        break;
+    case "suspenso" : 
+        console.log("Demonio negro\nManhunter\nBuscando...");
+        break;
+    case "terror" :
+        console.log("La Monja\nEvil Dead\nHuesera");
+        break;
+    default :
+        console.log("No existe esa categoria de pelicula");
+        break;
+}
